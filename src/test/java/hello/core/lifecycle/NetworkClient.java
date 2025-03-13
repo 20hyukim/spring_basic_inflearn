d9f44be5632a81d8e105a73a4ed05007e8ed03ed
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2025-03-13 21:14:21 +0900

    feat: 빈 등록 초기화, 소멸 메서드

---
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -0,0 +1,42 @@
+package hello.core.lifecycle;
+
+public class NetworkClient {
+
+
+    private String url;
+
+    public NetworkClient() {
+        System.out.println("생성자 호출, url = " + url);
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    // 서비스 시작시 호출
+    public void connect() {
+        System.out.println("connect:  " + url);
+    }
+
+    public void call(String message) {
+        System.out.println("call: " + url + "message = " + message);
+    }
+
+    // 서비스 종료시 호출
+    public void disconnect() {
+        System.out.println("close: " + url);
+    }
+
+    public void init() {
+        // 프로퍼티들 세팅이 끝나면 호출해 주겠다. (의존 관계 주입이 끝나면)
+        System.out.println("NetworkClient.init");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+
+    public void close() throws Exception {
+        System.out.println("NetworkClient.close");
+        disconnect();
+    }
+}
